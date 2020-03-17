128796810163d6cd7a17782a0465e4a1dcaad41d
unknown <783545134@qq.com>
2020-03-17 18:22:44 +0800

    first commit

---
+++ src/test/java/com/zhaowei/MajiangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaowei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
